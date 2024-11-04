01549115f00ac3c9e915fab5b9094c755a6a2811
lotusofc <131186587+lotusofc@users.noreply.github.com>
2024-11-04 21:44:07 +1100

    created button and colour library

---
+++ colours.js
@@ -0,0 +1,16 @@
+// colors.js
+const lightTheme = {
+    primary: '#7ad1c2',
+    secondary: '#9e5dd4',
+    text: '#006e62',
+    // Other light theme colors
+  };
+  
+const darkTheme = {
+
+};
+
+export default {
+    light: lightTheme,
+    dark: darkTheme,
+};
