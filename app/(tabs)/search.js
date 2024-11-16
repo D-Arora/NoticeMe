17b3f3b9c40365921f3661f1bdde60427cb20f37
RiceL123 <ericliao57@gmail.com>
2024-11-16 16:21:53 +1100

    💭

--- app/(tabs)/search.js
+++ app/(tabs)/search.js
@@ -1,9 +1,47 @@
-import { View, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import { defaultEvents, EVENTS_STORE_KEY } from "./events";
 
 export default function Search() {
+  const [events, setEvents] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [filterdEvents, setFilteredEvents] = useState([]);
+  useEffect(() => {
+    const getEventsFromAsyncStorage = async () => {
+      const storedEvents = await AsyncStorage.getItem(EVENTS_STORE_KEY);
+      if (!storedEvents) {
+        setEvents(defaultEvents);
+        await AsyncStorage.setItem(
+          EVENTS_STORE_KEY,
+          JSON.stringify(defaultEvents)
+        );
+      } else {
+        setEvents(JSON.parse(storedEvents));
+      }
+    };
+
+    getEventsFromAsyncStorage();
+  }, []);
+
+  useEffect(() => {
+    if (searchInput == "") {
+      setFilteredEvents(events);
+    } else {
+      setFilteredEvents(events.filter(x => x.title.toLowerCase().includes(searchInput.toLowerCase())));
+    }
+  }, [searchInput])
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Search Screen</Text>
+      <TextInput placeholder="Search" defaultValue={""} onChangeText={x => setSearchInput(x)}/>
+
+      <View style={{ display: "flex", flexDirection: "column", gap: "10" }}>
+        {filterdEvents.map((x) => (
+          <View>{JSON.stringify(x)}</View>
+        ))}
+      </View>
     </View>
   );
 }
